06d4ba22f73a3cfd3974d437b12f9394e6a9bcaa
Bryan Dina <bdina@dyn.com>
2013-12-05 15:56:24 -0500

    rough out the pipe delivery milter PoC

--- src/java/com/dyndns/email/postfix/milter/PostfixMilter.java
+++ src/java/com/dyndns/email/postfix/milter/PostfixMilter.java
@@ -1,7 +1,16 @@
 package com.dyndns.email.postfix.milter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PostfixMilter {
-    public static void main(String [] args) {
-        System.out.println("42");
+    public static void main(String [] args) throws Exception {
+        final BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s = r.readLine()) != null) {
+            System.out.println(s);
+        }
+
+        r.close();
     }
 }
