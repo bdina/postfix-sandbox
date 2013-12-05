5f8d25230f65c017f172e196f99756f0e80a12b0
Bryan Dina <bdina@dyn.com>
2013-12-05 17:14:37 -0500

    BROKEN: need to fix the classpath for my ant build

--- src/java/com/dyndns/email/postfix/milter/PostfixMilter.java
+++ src/java/com/dyndns/email/postfix/milter/PostfixMilter.java
@@ -1,17 +1,22 @@
 package com.dyndns.email.postfix.milter;
 
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
+import javax.mail.MimeMessage;
+import javax.mail.Session;
 
 public class PostfixMilter {
     public static void main ( String [] args ) throws Exception {
-        final BufferedReader stdin_reader = new BufferedReader ( new InputStreamReader( System.in ) );
+        final BufferedReader stdin = new InputStreamReader( System.in );
 
-        String line_of_input;
-        while ( ( line_of_input = stdin_reader.readLine() ) != null ) {
-            System.out.println( line_of_input );
-        }
+        final Message message = parseMessage ( stdin );
 
         stdin_reader.close ();
+    }
+
+    private static MimeMessage parseMessage ( final InputStream in_stream ) {
+        final Session mail_session = Session.getDefaultInstance( new Properties () );
+        final MimeMessage message = new Message ( mail_session, in_stream );
+        return message;
     }
 }
