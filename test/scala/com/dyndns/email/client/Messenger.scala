1dee4e6eb9f55f33ffcb8ab251024d7886139e3a
Bryan Dina <bryansdina@gmail.com>
2013-12-22 17:21:03 -0500

    made test client more flexible, can choose server and count of messages

--- test/scala/com/dyndns/email/client/Messenger.scala
+++ test/scala/com/dyndns/email/client/Messenger.scala
@@ -6,17 +6,33 @@ import javax.mail._
 import javax.mail.internet._
 
 object Messenger extends App {
-  val properties = new Properties()
-  properties.put("mail.smtp.host", "postfix.local")
-  properties.put("mail.smtp.port", "25")
+  override def main(args: Array[String]) {
+    val smtp_host = args(0)
+    val username = args(1)
+    val password = args(2)
+    val message_count = Integer.valueOf(args(3))
 
-  val session = Session.getInstance(properties)
+    val properties = new Properties()
+    properties.put("mail.smtp.host",        smtp_host)
+    properties.put("mail.smtp.port",        "25")
+    properties.put("mail.smtp.auth",        "true")
+    properties.put("mail.smtp.sasl.enable", "true")
 
-  val message = new MimeMessage(session)
-  message.setSubject("Testing")
-  message.setFrom(new InternetAddress("test@postfix.local"))
-  message.setRecipient(Message.RecipientType.TO, new InternetAddress("test@smtpsink.local"))
-  message.setText("This is a test")
+    val session = Session.getInstance(properties)
+    val transport = session.getTransport("smtp")
 
-  Transport.send(message)
+    transport.connect(username, password)
+
+    val message = new MimeMessage(session)
+    message.setSubject("Testing")
+    message.setFrom(new InternetAddress(s"$username@postfix.local"))
+    message.setRecipient(Message.RecipientType.TO, new InternetAddress("test@smtpsink.local"))
+    message.setText("This is a test")
+
+    for ( _ <- 0 to message_count) {
+      transport.sendMessage(message, message.getAllRecipients())
+    }
+
+    transport.close()
+  }
 }
